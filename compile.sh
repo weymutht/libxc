4e717b669cd8ec8b8523ecf8d56b19e9571e8123
Vera von Burg <vvburg@phys.chem.ethz.ch>
2021-04-07 14:53:30 +0200

    Make slight changes in compile, main to accommodate new GWS functional

--- compile.sh
+++ compile.sh
@@ -2,14 +2,26 @@
 
 set -e
 
-# Create C code of PBEHSERI from Maple code
 module load maple/2018.1
 module load perl/5.30.2
-cd maple/gga_exc
-../../scripts/maple2c.pl ../.. gga_c_pbehseri 4   # Takes about 44 minutes on tc-ws-006
+
+mapleDir=${HOME}/code/libxc/maple
+#cd ${mapleDir}/gga_exc
+## Create C code of GWS from Maple code
+#../../scripts/maple2c.pl ../.. gga_c_gws 4
+#../../scripts/maple2c.pl ../.. gga_x_gws 4
+#cd ${mapleDir}/lda_exc
+#../../scripts/maple2c.pl ../.. lda_c_pmgb06 4
+#../../scripts/maple2c.pl ../.. lda_x_erf_jt 4
+#cd ../../
+
+#cd ${mapleDir}/gga_exc
+#../../scripts/maple2c.pl ../.. gga_c_pbehseri 4   # Takes about 44 minutes on tc-ws-006
+#cd ../lda_exc
+#cd ${mapleDir}/lda_exc
 #../../scripts/maple2c.pl ../.. lda_c_pmgb06 4
 #../../scripts/maple2c.pl ../.. lda_x_erf 4
-cd ../..
+#cd ../..
 
 # Create C code for references
 cd src
@@ -18,16 +30,17 @@ cd ..
 
 # Compile, test, and install library; clean up afterwards (takes about 4 minutes on tc-ws-006)
 #autoreconf -i -v
-#./configure --prefix=/fast_scratch/tweymuth/cache/libxc/inst
-#make -j 4
+##./configure --prefix=/fast_scratch/vvburg/cache/libxc/inst
+./configure --prefix=/home/cds/vvburg/code/libxc/inst
+make -j 4
+make check
+make install
+make clean
+make distclean
+
+#cd build_test
+#cmake DCMAKE_BUILD_TYPE=RELEASE -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=../inst ..
+#make -j 6
 #make check
 #make install
 #make clean
-#make distclean
-
-cd build_test
-cmake DCMAKE_BUILD_TYPE=RELEASE -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=../inst ..
-make -j 6
-#make check
-make install
-make clean
--- main.c
+++ main.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 #include <xc.h>
 
 int main()
@@ -9,7 +8,7 @@ int main()
   double rho[5] = {0.1, 0.2, 0.3, 0.4, 0.5};
   double sigma[5] = {0.2, 0.3, 0.4, 0.5, 0.6};
   double exc[5];
-  int i, vmajor, vminor, vmicro, func_id = 999; // Use func_id = 130 for standard PBE, 999 for new PBEHSERI
+  int i, vmajor, vminor, vmicro, func_id = 524; //999; // Use func_id = 130 for standard PBE, 999 for new PBEHSERI, 524 for X_WPBEH
 
 xc_version(&vmajor, &vminor, &vmicro);
 printf("Libxc version: %d.%d.%d\n", vmajor, vminor, vmicro);
@@ -19,12 +18,46 @@ fprintf(stderr, "Functional '%d' not found\n", func_id);
 return 1;
 }
 
+printf("Printing info: \n");
+int  npar = xc_func_info_get_n_ext_params(func.info);
+  if(npar > 0) {
+    printf("\nFunctional has %i external parameters:\n",npar);
+    printf("%3s %13s %8s %s\n","idx","value","name","description");
+    for(i = 0; i < npar; i++)
+      printf("%3i % e %8s %s\n", i,
+             xc_func_info_get_ext_params_default_value(func.info, i),
+             xc_func_info_get_ext_params_name(func.info, i),
+             xc_func_info_get_ext_params_description(func.info, i));
+  } else {
+    printf("\nFunctional has no external parameters.\n");
+  }
+
+
 // Get default value of mu
-double mu = xc_func_info_get_ext_params_default_value(func.info, 3);
+double mu = xc_func_info_get_ext_params_default_value(func.info, 0);
 printf("Default value of mu: %f\n", mu);
+
+for (int j=0;j<4;j++){
 // Modify value of mu
-mu = 1.0;
+switch(j)
+{
+  case 0: mu = 0.0;
+          printf("mu now: %lf", mu);
+          break;
+  case 1: mu = 0.4;
+          printf("mu now: %lf", mu);
+          break;
+  case 2: mu = 10;
+          printf("mu now: %lf", mu);
+          break;
+  case 3: mu = 1000000;
+          printf("mu now: %lf", mu);
+          break;
+}
+printf("Setting mu: \n");
 xc_func_set_ext_params_name(&func, "_mu", mu);
+printf("Setting omega: \n");
+xc_func_set_ext_params_name(&func, "_omega", mu);
 
 switch(func.info->family)
 {
@@ -36,9 +69,11 @@ case XC_FAMILY_HYB_GGA:
 xc_gga_exc(&func, 5, rho, sigma, exc);
 break;
 }
-
+printf("\n");
 for(i=0; i<5; i+=1){
 printf("%lf %lf\n", rho[i], exc[i]);
+}
+
 }
 
 xc_func_end(&func);
