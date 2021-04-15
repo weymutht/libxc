0b5a5dd8742533972c1793e62e3a58aa59fe5626
Vera von Burg <vvburg@phys.chem.ethz.ch>
2021-04-15 10:22:33 +0200

    Modify compile script for newly added functionals

--- compile.sh
+++ compile.sh
@@ -5,30 +5,30 @@ set -e
 module load maple/2018.1
 module load perl/5.30.2
 
+libxcDir=${HOME}/code/libxc
 mapleDir=${HOME}/code/libxc/maple
-#cd ${mapleDir}/gga_exc
-## Create C code of GWS from Maple code
-#../../scripts/maple2c.pl ../.. gga_c_gws 4
-#../../scripts/maple2c.pl ../.. gga_x_gws 4
-#cd ${mapleDir}/lda_exc
-#../../scripts/maple2c.pl ../.. lda_c_pmgb06 4
-#../../scripts/maple2c.pl ../.. lda_x_erf_jt 4
-#cd ../../
+cd ${mapleDir}/gga_exc
+## Create C code of GWS and helper functionals from Maple code
+../../scripts/maple2c.pl ../.. gga_c_gws 4
+../../scripts/maple2c.pl ../.. gga_x_gws 4
+cd ${mapleDir}/lda_exc
+../../scripts/maple2c.pl ../.. lda_c_pmgb06 4
+../../scripts/maple2c.pl ../.. lda_x_erf_jt 4
+cd ${libxcDir}
 
 #cd ${mapleDir}/gga_exc
 #../../scripts/maple2c.pl ../.. gga_c_pbehseri 4   # Takes about 44 minutes on tc-ws-006
-#cd ../lda_exc
 #cd ${mapleDir}/lda_exc
 #../../scripts/maple2c.pl ../.. lda_c_pmgb06 4
 #../../scripts/maple2c.pl ../.. lda_x_erf 4
-#cd ../..
+#cd ${libxcDir}
 
 # Create C code for references
-cd src
+cd ${libxcDir}/src
 ../scripts/get_references.pl ../libxc.bib
-cd ..
+cd ${libxcDir}
 
-# Compile, test, and install library; clean up afterwards (takes about 4 minutes on tc-ws-006)
+# Compile, test, and install library; clean up afterwards
 #autoreconf -i -v
 ##./configure --prefix=/fast_scratch/vvburg/cache/libxc/inst
 ./configure --prefix=/home/cds/vvburg/code/libxc/inst
