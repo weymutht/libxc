e26e40a1e1b34906c56fb570e4aa437487e1b15f
Vera von Burg <vvburg@phys.chem.ethz.ch>
2021-04-16 11:43:20 +0200

    Fix important typo

--- src/gga_x_gws.c
+++ src/gga_x_gws.c
@@ -56,7 +56,7 @@ gws_lambda_set_ext_params(xc_func_type *p, const double *ext_params)
 #include "maple2c/gga_exc/gga_x_gws.c"
 #include "work_gga.c"
 
-#ifdef __cpluplus
+#ifdef __cplusplus
 extern "C"
 #endif
 const xc_func_info_type xc_func_info_gga_x_gws = {
